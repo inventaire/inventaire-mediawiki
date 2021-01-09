21dde370cec8eadaddb528f6d644e1c403840716
maxlath <k@maxlath.eu>
2021-01-09 20:17:40 +0100

    entrypoint: avoid to throw if ADMIN_* variables aren't set
    requiring those variables only when doing a local install

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Test if required environment variables have been set
-REQUIRED_VARIABLES=(ADMIN_NAME ADMIN_PASS ADMIN_EMAIL SECRET_KEY MYSQL_SERVER MYSQL_USER MYSQL_PASSWORD MYSQL_DATABASE)
+REQUIRED_VARIABLES=(SECRET_KEY MYSQL_SERVER MYSQL_USER MYSQL_PASSWORD MYSQL_DATABASE)
 for i in ${REQUIRED_VARIABLES[@]}; do
     eval THISSHOULDBESET=\$$i
     if [ -z "$THISSHOULDBESET" ]; then
-    echo "$i is required but isn't set. You should pass it to docker. See: https://docs.docker.com/engine/reference/commandline/run/#set-environment-variables--e---env---env-file";
+    echo "$i is required but isn't set. You should pass it to docker via the .mw_env file";
     exit 1;
     fi
 done
@@ -26,6 +26,17 @@ fi
 
 # Do the mediawiki install (only if LocalSettings doesn't already exist)
 if [ ! -e "/var/www/html/LocalSettings.php" ]; then
+
+    # Test additionnaly required environment variables for the first run
+    REQUIRED_INSTALLATION_VARIABLES=(ADMIN_NAME ADMIN_PASS ADMIN_EMAIL)
+    for i in ${REQUIRED_INSTALLATION_VARIABLES[@]}; do
+        eval THISSHOULDBESET=\$$i
+        if [ -z "$THISSHOULDBESET" ]; then
+        echo "$i is required but isn't set. You should pass it to docker via the .mw_env file";
+        exit 1;
+        fi
+    done
+
     # Setup the database (needs to be done before copying our custom LocalSettings.php)
     php /var/www/html/maintenance/install.php --dbuser $MYSQL_USER --dbpass $MYSQL_PASSWORD --dbname $MYSQL_DATABASE --dbserver $MYSQL_SERVER --lang $SITE_LANG --pass $ADMIN_PASS $SITE_NAME $ADMIN_NAME
     php /var/www/html/maintenance/resetUserEmail.php --no-reset-password $ADMIN_NAME $ADMIN_EMAIL
