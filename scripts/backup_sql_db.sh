59171217067258936075a60a15976d01ce0336e3
maxlath <k@maxlath.eu>
2021-03-03 11:04:40 +0100

    backup_sql_db: fix cleanup

--- scripts/backup_sql_db.sh
+++ scripts/backup_sql_db.sh
@@ -7,11 +7,13 @@ echo "$(date --iso-8601=seconds): starting SQL backup"
 # Allow to pass a destination folder as unique argument, defaults to $PWD
 project_root=${1:-$PWD}
 echo "project_root: $project_root"
-backup_folder="${project_root}/backups/$(date -I)"
+backups_folder="${project_root}/backups"
+echo "backups_folder: $backups_folder"
+backup_folder="${backups_folder}/$(date -I)"
 echo "backup_folder: $backup_folder"
 mkdir -p "$backup_folder"
-rm -f "${project_root}/backups/latest"
-ln -s "$backup_folder" "${project_root}/backups/latest"
+rm -f "${backups_folder}/latest"
+ln -s "$backup_folder" "${backups_folder}/latest"
 
 # Set $wgReadOnly
 echo "\$wgReadOnly = \"Ongoing database backup, Access will be restored shortly\";" >> "${project_root}/LocalSettings.php"
@@ -26,6 +28,6 @@ docker exec inventaire-mediawiki_web_1 sh -c 'sed "/wgReadOnly/d" "/var/www/html
 
 # Cleanup old backups:
 ## Delete backups more than 10 days old, unless they are from the first of the month
-find "$backup_folder" -mindepth 1 -type d -mtime +10 -not -regex ".*01$" -exec rm -rf {} \;
+find "$backups_folder" -mindepth 1 -type d -mtime +10 -not -regex ".*01$" -exec rm -rf {} \;
 ## Delete backups from the first of each months once passed 6 months
-find "$backup_folder" -mindepth 1 -type d -mtime +180 -exec rm -rf {} \;
+find "$backups_folder" -mindepth 1 -type d -mtime +180 -exec rm -rf {} \;
