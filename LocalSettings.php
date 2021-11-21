94edad53ffe698f14d7df5bfe96104e7aa83dc94
maxlath <k@maxlath.eu>
2021-11-21 14:17:37 +0100

    LocalSettings.php: set $wgCookieSecure = true
    addressing https://git.radicallyopensecurity.com/nlnet/ngid-inventaire/-/issues/13

--- LocalSettings.php
+++ LocalSettings.php
@@ -179,6 +179,8 @@ $wgOAuth2Client['configuration']['scopes'] = 'stable-username email';
 $wgOAuth2Client['configuration']['service_name'] = $wgSitename;
 $wgOAuth2Client['configuration']['service_login_link_text'] = "Login with Inventaire";
 
+$wgCookieSecure = true;
+
 wfLoadExtension( 'Babel' );
 wfLoadExtension( 'Cite' );
 wfLoadExtension( 'cldr' );
